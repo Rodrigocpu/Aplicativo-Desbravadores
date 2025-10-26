d46dd34094ed4bca4a9d7a76933ceac25b66f144
Rodrigocpu <rodirogkioa@gmail.com>
2025-10-25 23:17:16 -0300

    Exclui sem querer
    acabei de excluir 5 pastas que acabei de codar, ja foi feita a correção

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getProfile } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/me", protect, getProfile);
-
-export default router;
